aab8c927a3856fdc980913a5c61d722485438193
snowcloud <derek@snowcloud.co.uk>
2010-10-18 13:26:16 +0100

    added initial content to setup files

---
+++ README.txt
@@ -0,0 +1,1 @@
+Shared code for ClinicalTemplates.org web framework.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ClinicalTemplate.org shared code',
+    version='0.1',
+    packages=['ct_framework',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
